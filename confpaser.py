5b97bf4a3d0e6369aa627da43c13d69070ac6655
Agustin Nunez <anunez@gmail.com>
2018-07-01 20:54:32 +0000

    Use an external configuration file

---
+++ confpaser.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+import ConfigParser
+
+debug=True
+Config = ConfigParser.ConfigParser()
+Config.read("ipbot.conf")
+if debug: print "Config sections: ",Config.sections()
+iplist = []
+ipname = []
+
+try:
+  ssid = Config.get("wifi","ssid")
+  pswd = Config.get("wifi","pswd")
+except:
+  print "format eror in  [wifi] configuration" 
+if debug: print "ssid: %s, pass: '%s" % (ssid, pswd)
+
+try:
+  for name in Config.options("iplist"):
+    ip=Config.get("iplist",name)
+    iplist.extend([ip])
+    ipname.extend([name])
+
+    if debug: print "ip: %s, name: %s" % (ip,ipname)
+except:
+  print "format error in [iplist] section"
+
+print "all ok"
+print ipname
+print iplist
+
+
--- ipbot.py
+++ ipbot.py
@@ -5,30 +5,58 @@ import datetime
 import os
 import sys
 import time
+import ConfigParser
 
 debug    = True # Turn into True to print debug info on terminal
-period   = 10  # cycle period in seconds
-retry    = 3
-deadline = 10
-timeout  = 10
+period   = 120  # cycle period in seconds
+retry    = 5
+deadline = 20
+timeout  = 20
+confile  = "ipbot.conf" # insert full path to a configuration file in a protected directory
+iplist = []
+ipname = []
 
-# list of ip, and is id names. List should be identical number of elements or only shorter is considered
-iplist = ['192.168.1.12','192.168.1.13'] # Mobile phone permanent lan ip list, can be any number of ip's
-ipname = ['Mobile1','Mobile2'] # Mobile phone permanent lan ip list, can be any number of ip's
+
+
+Config = ConfigParser.ConfigParser()
+Config.read(confile)
+if debug: print "Config sections: ",Config.sections()
+
+try:
+  ssid = Config.get("wifi","ssid")
+  pswd = Config.get("wifi","pswd")
+except:
+  print "format eror in  [wifi] section of '%s' configuration file" % confile
+if debug: print "ssid: %s, pass: '%s" % (ssid, pswd)
+
+try:
+  for name in Config.options("iplist"):
+    ip=Config.get("iplist",name)
+    iplist.extend([ip])
+    ipname.extend([name])
+    if debug: print "ip: %s, name: %s" % (ip,name)
+except:
+  print "format error in [iplist] section"
+if debug: print iplist,ipname
+
 statusnet = dict()
-
  
-#Define Username and Password of sender. I used a new gmail account
-username = 'senderemail@gmail.com'
-password = 'pass'
+#Define Username and Password of sender email account and receiver one(mailto)
+try:
+  username = Config.get("mail","username") # read user and password of smtp sender email account
+  password = Config.get("mail","password")
+  mailto   = Config.get("mail","mailto")   # read destination mail for sending messages
+  if debug: print "username: '%s' password '%s' mailto '%s'" % (username,password,mailto)
+except:
+  print "format error in [mail] section of '%s' configuration file" % confile
  
 def sendemail(result):
     if debug: 
         print "Sendmail simulated in debug mode. Turn debug to False to send real mails"
         return
     notifier = 'Bot'
-    sender = 'fake@gmail.com' # this is fake email from field
-    receivers = ['receiver@gmail.com'] #Use one or a list of email addresses separated by comma
+    sender = 'home@gmail.com' # this is fake email from field
+    receivers = ['anunez@gmail.com'] #Use one or a list of email addresses separated by comma
     date = datetime.datetime.now().strftime( '%m/%d/%Y %H:%M:%S' )
     header = 'To:' + notifier + '\n' + 'From: ' + sender + '\n' + 'Subject: Bot ' + result +'\n'
     message = header + '\n' + 'date:' + '\n' + date + '\n' + 'Change: ' + result
